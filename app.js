f09f62f2a4197bc8cbd0e001a075234ec74c97e2
Rohit <tomarrohit5034@gmail.com>
2022-11-16 07:10:41 +0000

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const addDays = require("date-fns/addDays");
 const app = express();
 app.get("/", (request, response) => {
-  const addDays = require("date-fns/addDays");
   let dateTime = addDays(new Date(), 100);
   response.send(
     `${dateTime.getDate()}/${dateTime.getMonth() + 1}/${dateTime.getFullYear()}`
