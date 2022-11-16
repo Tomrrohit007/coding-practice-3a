4fa2dedd5556c2e7af08391030033d7c331f9a59
Rohit <tomarrohit5034@gmail.com>
2022-11-16 07:07:00 +0000

    changes

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 app.get("/", (request, response) => {
   const addDays = require("date-fns/addDays");
   let dateTime = addDays(new Date(), 100);
-  response.send(dateTime);
+  response.send(
+    `${dateTime.getDate()}/${dateTime.getMonth() + 1}/${dateTime.getFullYear()}`
+  );
 });
 
 app.listen(3000);
